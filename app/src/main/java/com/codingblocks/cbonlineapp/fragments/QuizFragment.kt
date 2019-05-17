f33569dddfdf9822c9cdb505c5481533e70f394d
raystatic <rahul9650ray@gmail.com>
2019-05-17 22:37:14 +0530

    some more changes

--- app/src/main/java/com/codingblocks/cbonlineapp/fragments/QuizFragment.kt
+++ app/src/main/java/com/codingblocks/cbonlineapp/fragments/QuizFragment.kt
@@ -25,6 +25,7 @@ import com.codingblocks.cbonlineapp.util.RUN_ATTEMPT_ID
 import com.codingblocks.onlineapi.Clients
 import com.codingblocks.onlineapi.models.QuizSubmission
 import com.codingblocks.onlineapi.models.Question
+import com.codingblocks.onlineapi.models.QuizResult
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.firebase.analytics.FirebaseAnalytics
 import kotlinx.android.synthetic.main.bottom_question_sheet.*
@@ -76,7 +77,6 @@ class QuizFragment : Fragment(), AnkoLogger, ViewPager.OnPageChangeListener, Vie
 
         Clients.onlineV2JsonApi.getQuizById(quizId).enqueue(retrofitCallback { _, response ->
             response?.body()?.let { quiz ->
-                quiz.questions?.let { setUpQuestionBottomSheet(it) }
                 quiz.questions?.forEachIndexed { index, question ->
                     questionList[index] = question.id ?: ""
                     if (index == quiz.questions!!.size - 1) {
@@ -87,6 +87,7 @@ class QuizFragment : Fragment(), AnkoLogger, ViewPager.OnPageChangeListener, Vie
                                 quizViewPager.currentItem = 0
                                 quizViewPager.setOnPageChangeListener(this)
                                 quizViewPager.offscreenPageLimit = 3
+                                it.result?.let { it1 -> setUpQuestionBottomSheet(it1) }
                             }
                         })
 
@@ -107,7 +108,7 @@ class QuizFragment : Fragment(), AnkoLogger, ViewPager.OnPageChangeListener, Vie
         quizViewPager.currentItem = pos
     }
 
-    private fun setUpQuestionBottomSheet(questions: ArrayList<Question>) {
+    private fun setUpQuestionBottomSheet(questions: QuizResult) {
 
         choiceNumberAdapter = ChoicesAdapter(this)
         numbersRv.layoutManager = GridLayoutManager(context,3)
